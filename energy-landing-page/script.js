f5efe641fb0890dbc5a0b0a2029855afea346823
AnhQuocTran <anhquocntran@gmail.com>
2026-05-25 02:39:32 -0700

    Close mobile menu after navigation click

--- energy-landing-page/script.js
+++ energy-landing-page/script.js
@@ -1,6 +1,13 @@
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
+const navItems = document.querySelectorAll(".nav-links a");
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("show");
+});
+
+navItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    navLinks.classList.remove("show");
+  });
 });
